c8646e21e42f8dab990dbce4cd8706b3fe554ed2
Gideon Botha <gideon.botha@entelect.co.za>
2021-04-07 15:01:57 +0200

    init first app

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Gideon Botha on 2021/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
